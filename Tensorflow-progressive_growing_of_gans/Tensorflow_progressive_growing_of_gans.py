4dad79dc956ad19814c1636c5406eb9c861d061b
闫坤 <yankun1138283845@foxmail.com>
2018-02-05 20:49:33 +0800

    temp0

--- Tensorflow-progressive_growing_of_gans/Tensorflow_progressive_growing_of_gans.py
+++ Tensorflow-progressive_growing_of_gans/Tensorflow_progressive_growing_of_gans.py
@@ -11,9 +11,9 @@ import tensorflow as tf
 ###################################################################
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string("input_dir", ".", "Input directory where training dataset and meta data are saved")
-tf.app.flags.DEFINE_string("output_dir", ".", "Output directory where output such as logs are saved.")
-tf.app.flags.DEFINE_string("log_dir", ".", "Model directory where final model files are saved.")
+tf.app.flags.DEFINE_string("input_dir", "./data", "Input directory where training dataset and meta data are saved")
+tf.app.flags.DEFINE_string("output_dir", "./output", "Output directory where output such as logs are saved.")
+tf.app.flags.DEFINE_string("log_dir", "./log", "Model directory where final model files are saved.")
 
 def main(_):
     # TODO: add your code here
