2f4db7695e277a5a8d47a4dc9f84e6819f6e33b5
闫坤 <yankun1138283845@foxmail.com>
2018-02-22 01:21:12 +0800

    implement activation resize layer

--- Tensorflow-progressive_growing_of_gans/layers.py
+++ Tensorflow-progressive_growing_of_gans/layers.py
@@ -5,21 +5,42 @@ import numpy as np
 
 
 #----------------------------------------------------------------------------
-# Resize activation tensor 'v' of shape 'si' to match shape 'so'.
-
+# Resize activation tensor 'inputs' of shape 'si' to match shape 'so'.
 class ACTVResizeLayer(Layer):
-    def __init__(self,output_dim,**kwargs):
-        self.output_dim = output_dim
+    def __init__(self,si,so,**kwargs):
+        self.si = si
+        self.so = so
         super(ACTVResizeLayer,self).__init__(**kwargs)
-    def build(self,input_shape):
-        # TODO(naykun): Create a trainable weight variable for this layer.
-        pass
     def call(self, inputs, **kwargs):
         # TODO(naykun): Implementation layer function
-        pass
+        assert len(si) == len(so) and si[0] == so[0]
+
+        # Decrease feature maps.  Attention: channels last
+        if si[-1] > so[-1]:
+            inputs = inputs[...,:so[-1]]
+
+        # Increase feature maps Attention:channels last
+        if si[-1] < so[-1]:
+            z = K.zeros((so[:-1]+(so[-1]-si[-1])),dtype=inputs.dtype)
+            inputs = K.concatenate([inputs,z])
+        
+        # Shrink spatial axis 
+        if len(si) == 4 and (si[1] > so[1] or si[2] > so[2]):
+            assert si[1] % so[1] == 0 and si[2] % so[2] == 0
+            pool_size = (si[1] / so[1],si[2] / so[2])
+            strides = pool_size
+            outputs = K.pool2d(inputs,pool_size=pool_size,strides=strides,padding='same',data_format='channels_last',pool_mode='avg')
+
+        #Extend spatial axis
+        for i in range(1,len(si)-1):
+            if si[i] < so[i]:
+                assert so[i]%si[i] == 0
+                outputs = K.repeat_elements(inputs,rep=2,axis=i)
+
+        return outputs
     def compute_output_shape(self, input_shape):
-        # TODO(naykun): Help Keras can do automatic shape inference.
-        pass
+        # TODO(naykun): Help Keras do automatic shape inference.
+        return so
 
 
 #----------------------------------------------------------------------------
@@ -42,7 +63,6 @@ class LODSelectLayer(Layer):
 
 #----------------------------------------------------------------------------
 # Pixelwise feature vector normalization.
-
 class PixelNormLayer(Layer):
     def __init__(self,output_dim,**kwargs):
         self.output_dim = output_dim
@@ -62,7 +82,6 @@ class PixelNormLayer(Layer):
 
 #----------------------------------------------------------------------------
 # Applies equalized learning rate to the preceding layer.
-
 class WScaleLayer(Layer):
     def __init__(self,output_dim,**kwargs):
         self.output_dim = output_dim
@@ -79,10 +98,9 @@ class WScaleLayer(Layer):
 
 
 #----------------------------------------------------------------------------
-# Minibatch stat concatenation layer. 
+# Minibatch stat concatenation layer.
 # - func is the function to use for the activations across minibatch
 # - averaging tells how much averaging to use ('all', 'spatial', 'none')
-
 class MinibatchStatConcatLayer(Layer):
     def __init__(self,output_dim,**kwargs):
         self.output_dim = output_dim
@@ -99,9 +117,8 @@ class MinibatchStatConcatLayer(Layer):
 
 
 #----------------------------------------------------------------------------
-# Generalized dropout layer. Supports arbitrary subsets of axes and different
-# modes. Mainly used to inject multiplicative Gaussian noise in the network.
-
+# Generalized dropout layer.  Supports arbitrary subsets of axes and different
+# modes.  Mainly used to inject multiplicative Gaussian noise in the network.
 class GDropLayer(Layer):
     def __init__(self,output_dim,**kwargs):
         self.output_dim = output_dim
@@ -118,9 +135,8 @@ class GDropLayer(Layer):
 
 
 #----------------------------------------------------------------------------
-# Layer normalization. Custom reimplementation based on the paper:
+# Layer normalization.  Custom reimplementation based on the paper:
 # https://arxiv.org/abs/1607.06450
-
 class LayerNormLayer(Layer):
     def __init__(self,output_dim,**kwargs):
         self.output_dim = output_dim
